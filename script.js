fef2b9679dc2ba9bdb179c48d26ad9b3b1cdb30c
misschootemup <misschootemup@gmail.com>
2023-10-06 11:47:42 -0500

    added a fuction and a confirm

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-
+function generatePassword() {
+  var pslength = prompt("Password must be between 8-128 characters. How many characters would you prefer? ")
+  if (pslength < 8 || pslength > 128 || isNaN(pslength)) {
+    alert("Dummy 8-128 characters, Duh")
+    return "Please try again."
+  }
+  var lowercase = confirm (" Would you like lowercase letters in your password?")
+  var uppercase = confirm ("Would you like a uppercase letters in your password?")
+  var special = confirm ("Would you like a special character in your password?")
+  var number = confirm ("Would you like a number in your password?")
+}
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
